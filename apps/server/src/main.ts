949cfb9ce69491063ea7c3d2b92cddb3d4542861
Mateusz Łątka <mateusz.latka9@gmail.com>
2022-11-17 22:09:03 +0100

    deploy test 3

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -54,6 +54,7 @@ app.use(errorHandlerMiddleware);
 const port = process.env.port || 3333;
 
 const server = app.listen(port, () => {
+  console.log(__dirname);
   console.log(`Listening at http://localhost:${port}/`);
 });
 
