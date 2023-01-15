803776723049fcadf87635326a00e3f290028ba0
Mateusz Łątka <mateusz.latka9@gmail.com>
2023-01-15 22:42:27 +0100

    add .env.example file

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import { getJestProjects } from '@nrwl/jest';
 
-process.env.DATABASE_URL = 'postgres://postgres:postgrespw@localhost:49153/event_organizer_db';
+// process.env.DATABASE_URL = 'postgres://postgres:postgrespw@localhost:49153/event_organizer_db';
+process.env.DATABASE_URL = process.env.DATABASE_URL_TEST;
 
 export default {
   projects: getJestProjects(),
